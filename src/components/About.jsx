cc245447a616fa80ffaf688d4fabd9f3387f9c50
khalil khan <khalilkhankk3272711@gmail.com>
2023-08-16 10:07:54 +0500

    first commit to mobile app

---
+++ src/components/About.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <>
+    <p>this is from about page of mobile app</p></>
+  )
+}
+
+export default About
---
+++ src/components/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import About from './About'
+
+const Home = () => {
+  return (
+    <>
+    <h1>this is homepage of mobile app</h1>
+    <About/>
+    
+    </>
+  )
+}
+
+export default Home
