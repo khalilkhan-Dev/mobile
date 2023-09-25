250c923253be2476e2c844b3cb161f790ac42124
khalil khan <khalilkhankk3272711@gmail.com>
2023-09-25 07:55:49 +0500

    add loader

--- src/App.js
+++ src/App.js
@@ -1,13 +1,33 @@
 // import logo from './logo.svg';
+import { useEffect, useState } from 'react';
 import './App.css';
 import Home from './components/Home';
+import Loader from './components/loading/Loader';
+
+
+
 
 function App() {
-  return (
-    <>
-    <Home/>
-    </>
-  );
+  const [isLoading, setIsLoading] = useState(true);
+
+
+
+  useEffect(()=>{
+    const fakeDataFetch = ()=>{
+      setTimeout(()=>{
+        setIsLoading(false);
+      },2000)
+    }
+    fakeDataFetch();
+  },[])
+
+  return isLoading ? 
+   
+
+     <Loader/> :  <Home/>;
+   
+   
+  
 }
 
 export default App;
---
+++ src/components/loading/Loader.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import '../loading/Loader.css'
+
+const Loader = () => {
+  return (
+    <>
+    <div className='loader'>
+        <h3>khalil khan</h3>
+    </div>
+    
+    </>
+  )
+}
+
+export default Loader
