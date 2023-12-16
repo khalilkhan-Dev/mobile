0546808bf0ad84646aad935786f2c11dc1f573f2
khalil khan <khalilkhankk3272711@gmail.com>
2023-12-16 12:32:18 +0500

    adding custom domain

--- src/components/loading/Loader.jsx
+++ src/components/loading/Loader.jsx
@@ -4,9 +4,13 @@ import '../loading/Loader.css'
 const Loader = () => {
   return (
     <>
+  
+
     <div className='loader'>
+      <h6>Wellcome to</h6>
         <h3>khalil khan</h3>
     </div>
+ 
     
     </>
   )
